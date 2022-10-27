34ee3e4a8373bc9fd893a129b6ce220302b7c20f
polyakhtar <poly09876pm@gmail.com>
2022-10-27 22:37:18 +0600

    navbar logo designed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/Authprovider/Authprovider';
+import picture from '../Assets/picture.jpg'
 const Header = () => {
   const [open,setOpen]=useState(false);
   const {user,logOut}=useContext(AuthContext);
@@ -11,7 +12,9 @@ const Header = () => {
   }
   return (
     <div className="navbar bg-primary text-primary-content">
-  <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+      
+  <Link className="btn btn-ghost normal-case text-xl d-flex"><img style={{height:'30px',weight:'30px',marginRight:'10px'}}src={picture} alt=""/>Computer Learning Club</Link>
+  
   <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
    <Link className="btn btn-ghost normal-case text-xl" to='/courses'>Courses</Link>
    <Link className="btn btn-ghost normal-case text-xl" to='/about'>About</Link>
